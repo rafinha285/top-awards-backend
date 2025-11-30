d634fd238466ab164b498cd28b86f591d3a5db63
abacate <rafael.schrott02@gmail.com>
2025-11-30 20:39:10 -0300

    inital commit

---
+++ src/main/java/org/animefoda/topawardsbackend/TopawardsBackendApplication.java
@@ -0,0 +1,13 @@
+package org.animefoda.topawardsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopawardsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TopawardsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/animefoda/topawardsbackend/TopawardsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.animefoda.topawardsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopawardsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
